8ab4f11ac02d7dead8d54c9bcd46ef35998fb6f2
Tomas Sirio <tomas.sirio@leniolabs.com>
2019-10-15 13:34:51 -0300

    First Commit

---
+++ src/main/java/com/tomassirio/ataccamascanner/AtaccamaScannerApplication.java
@@ -0,0 +1,13 @@
+package com.tomassirio.ataccamascanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaccamaScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaccamaScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomassirio/ataccamascanner/AtaccamaScannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomassirio.ataccamascanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtaccamaScannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
