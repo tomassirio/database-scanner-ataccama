d93fb675e7dd7478ed8e54d1715793caca05fedc
Tomas Sirio <tomassirio@gmail.com>
2019-10-15 22:00:25 -0300

    - Added a Structure to retrieve the Databases data in a hierarchy     - DatabaseStructure     - Schema     - Table     - Column - Started the development on the databaseStructure retrieval

--- src/main/java/com/tomassirio/ataccamascanner/service/DataSourceService.java
+++ src/main/java/com/tomassirio/ataccamascanner/service/DataSourceService.java
@@ -1,10 +1,15 @@
 package com.tomassirio.ataccamascanner.service;
 
 import com.tomassirio.ataccamascanner.model.DTO.InstanceInfoDTO;
+import com.tomassirio.ataccamascanner.model.InstanceInfo;
+import com.tomassirio.ataccamascanner.model.database.DatabaseStructure;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 public interface DataSourceService {
 
-    DataSource getDataSource(InstanceInfoDTO instanceInfoDTO) throws Exception;
+    DataSource getDataSource(InstanceInfo instanceInfo);
+
+    DatabaseStructure getDatabaseStructure(InstanceInfoDTO instanceInfoDTO) throws SQLException;
 }
--- src/main/java/com/tomassirio/ataccamascanner/service/DataSourceServiceImpl.java
+++ src/main/java/com/tomassirio/ataccamascanner/service/DataSourceServiceImpl.java
@@ -1,24 +1,65 @@
 package com.tomassirio.ataccamascanner.service;
 
 
+import com.tomassirio.ataccamascanner.config.CommonConfiguration;
 import com.tomassirio.ataccamascanner.model.DTO.InstanceInfoDTO;
-import lombok.extern.slf4j.Slf4j;
+import com.tomassirio.ataccamascanner.model.InstanceInfo;
+import com.tomassirio.ataccamascanner.model.database.DatabaseStructure;
+import com.tomassirio.ataccamascanner.repository.InstanceInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-@Slf4j
 @Service
 public class DataSourceServiceImpl implements DataSourceService {
 
     private static final String MYSQL_JDBC = "jdbc:mysql";
 
+    @Autowired
+    private InstanceInfoRepository instanceInfoRepository;
+
+    @Autowired
+    private CommonConfiguration commonConfiguration;
+
     @Override
-    public DataSource getDataSource(InstanceInfoDTO instanceInfoDTO) {
+    public DataSource getDataSource(InstanceInfo instanceInfo) {
         return DataSourceBuilder.create()
-                .url(MYSQL_JDBC + "://" + instanceInfoDTO.getHost() + ":" + instanceInfoDTO.getPort())
-                .username(instanceInfoDTO.getUser())
-                .password(instanceInfoDTO.getPassword()).build();
+                .url(MYSQL_JDBC + "://" + instanceInfo.getHost() + ":" + instanceInfo.getPort())
+                .username(instanceInfo.getUser())
+                .password(instanceInfo.getPassword()).build();
     }
+
+    @Override
+    public DatabaseStructure getDatabaseStructure(InstanceInfoDTO instanceInfoDTO) throws SQLException {
+        Connection connection = null;
+        try{
+            InstanceInfo instanceInfo = instanceInfoRepository.findByInstanceName(instanceInfoDTO.getInstanceName());
+
+            DataSource dataSource = getDataSource(instanceInfo);
+
+            connection = dataSource.getConnection();
+
+            String query = getQuery(commonConfiguration.getStructureFileMySql());
+
+
+
+        }catch (Exception e){
+            throw e;
+        }
+        return null;
+    }
+
+    private String getStructureFileName() {
+        return commonConfiguration.getStructureFileMySql();
+    }
+
+    private String getQuery(String filename){
+        return "";
+    }
+
 }
